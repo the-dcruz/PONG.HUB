dddddbf3c41bf496927e7cfffffe034009f42a02
Daniel Cruz <cruzda2000@gmail.com>
2014-04-29 22:05:02 -0700

    Login view should work now

---
+++ PONG.HUB/LoginControllerViewController.h
@@ -0,0 +1,15 @@
+//
+//  LoginControllerViewController.h
+//  PONG.HUB
+//
+//  Created by Daniel Cruz on 4/29/14.
+//  Copyright (c) 2014 PONG HUB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginControllerViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *username;
+@property (weak, nonatomic) IBOutlet UITextField *password;
+
+@end
--- PONG.HUB/phubAppDelegate.m
+++ PONG.HUB/phubAppDelegate.m
@@ -1,4 +1,4 @@
-//
+    //
 //  phubAppDelegate.m
 //  PONG.HUB
 //
@@ -12,10 +12,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
-    self.window.backgroundColor = [UIColor whiteColor];
-    [self.window makeKeyAndVisible];
     return YES;
 }
 
