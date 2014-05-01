1bd2413aaed924b68b1869dce55d9d82450f1171
SeanPryor <seanppryor@gmail.com>
2014-04-30 21:59:08 -0700

    Daniel figured out fonts, gg

---
+++ PONG.HUB/CustomUILabel.h
@@ -0,0 +1,13 @@
+//
+//  CustomUILabel.h
+//  PONG.HUB
+//
+//  Created by Sean Pryor on 4/30/14.
+//  Copyright (c) 2014 PONG HUB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomUILabel : UILabel
+
+@end
