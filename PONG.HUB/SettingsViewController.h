aa10c4f684a6207f68950e91e90313861f339072
Daniel Cruz <cruzda2000@gmail.com>
2014-05-11 23:46:35 -0700

    Logout button in settings page

---
+++ PONG.HUB/SettingsViewController.h
@@ -0,0 +1,15 @@
+//
+//  SettingsViewController.h
+//  PONG.HUB
+//
+//  Created by Daniel Cruz on 5/7/14.
+//  Copyright (c) 2014 PONG HUB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <FacebookSDK/FacebookSDK.h>
+
+@interface SettingsViewController : UIViewController <FBLoginViewDelegate,UINavigationControllerDelegate>
+@property (weak, nonatomic) IBOutlet FBLoginView *fbLoginView;
+
+@end
---
+++ PONG.HUB/SettingsViewController.m
@@ -0,0 +1,56 @@
+//
+//  SettingsViewController.m
+//  PONG.HUB
+//
+//  Created by Daniel Cruz on 5/7/14.
+//  Copyright (c) 2014 PONG HUB. All rights reserved.
+//
+
+#import "SettingsViewController.h"
+
+@interface SettingsViewController ()
+
+@end
+
+@implementation SettingsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.fbLoginView.delegate = self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)loginViewShowingLoggedOutUser:(FBLoginView *)loginView {
+    // test to see if we can use the share dialog built into the Facebook application
+    FBLinkShareParams *p = [[FBLinkShareParams alloc] init];
+    p.link = [NSURL URLWithString:@"http://developers.facebook.com/ios"];
+    [self.view.window.rootViewController dismissViewControllerAnimated:YES completion:nil];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
