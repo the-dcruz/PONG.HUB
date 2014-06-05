c487bc2022998dc02e9ccc921546327e87b5cc93
SeanPryor <seanppryor@gmail.com>
2014-06-05 16:08:39 -0700

    added rows to table, other small edits

--- PONG.HUB/ProfileViewController.m
+++ PONG.HUB/ProfileViewController.m
@@ -38,7 +38,7 @@
 }
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 3;
+    return 11;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -56,10 +56,29 @@
         NSData *data = [NSData dataWithContentsOfURL:url];
         UIImage *img = [[UIImage alloc] initWithData:data];
         [((UserProfileTableViewCell*)cell).picture setImage:img];
+        
     } else if (indexPath.row == 1) {
         cell = [tableView dequeueReusableCellWithIdentifier:@"statsNav" forIndexPath:indexPath];
     } else if (indexPath.row == 2) {
         cell = [tableView dequeueReusableCellWithIdentifier:@"pointerCell" forIndexPath:indexPath];
+    } else if (indexPath.row == 3) {
+        cell = [tableView dequeueReusableCellWithIdentifier:@"cupRatio" forIndexPath:indexPath];
+        //not sure how to make this font size change.
+        cell.textLabel.font = [UIFont systemFontOfSize:8.0];
+    } else if (indexPath.row == 4) {
+        cell = [tableView dequeueReusableCellWithIdentifier:@"mainStats" forIndexPath:indexPath];
+    } else if (indexPath.row == 5) {
+        cell = [tableView dequeueReusableCellWithIdentifier:@"gamesPlayed" forIndexPath:indexPath];
+    } else if (indexPath.row == 6) {
+        cell = [tableView dequeueReusableCellWithIdentifier:@"winPercent" forIndexPath:indexPath];
+    } else if (indexPath.row == 7) {
+        cell = [tableView dequeueReusableCellWithIdentifier:@"dividerLine" forIndexPath:indexPath];
+    } else if (indexPath.row == 8) {
+        cell = [tableView dequeueReusableCellWithIdentifier:@"cupsSank" forIndexPath:indexPath];
+    } else if (indexPath.row == 9) {
+        cell = [tableView dequeueReusableCellWithIdentifier:@"beersDrank" forIndexPath:indexPath];
+    } else if (indexPath.row == 10) {
+        cell = [tableView dequeueReusableCellWithIdentifier:@"endCell" forIndexPath:indexPath];
     }
     
     return cell;
@@ -73,6 +92,22 @@
         return 55;
     else if (indexPath.row == 2)
         return 15;
+    else if (indexPath.row == 3)
+        return 20;
+    else if (indexPath.row == 4)
+        return 90;
+    else if (indexPath.row == 5)
+        return 25;
+    else if (indexPath.row == 6)
+        return 40;
+    else if (indexPath.row == 7)
+        return 2;
+    else if (indexPath.row == 8)
+        return 23;
+    else if (indexPath.row == 9)
+        return 30;
+    else if (indexPath.row == 10)
+        return 20;
     return 0;
 }
 
